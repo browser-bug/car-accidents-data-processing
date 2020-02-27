01122ce7068577f63b347fc26ce233d83e3fff30
Bernardo <bernardo.menicagli@gmail.com>
2020-02-27 18:56:17 +0100

    Added duration statistics using cpuSecond().

--- OpenMP/serial.cc
+++ OpenMP/serial.cc
@@ -40,12 +40,24 @@ int main()
         map<int, int>()  // 2017
     };
 
-    double begin = cpuSecond();
+    double overallBegin = cpuSecond();
 
-    // Number of lethal accidents per week throughout the entire dataset. Using index: NUMBER_OF_PERSONS_KILLED
+    // [1] Loading data from file
+    double loadBegin = cpuSecond();
+
+    vector<CSVRow> dataset;
     for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
+        dataset.push_back((*loop));
+
+    double loadDuration = cpuSecond() - loadBegin;
+    printf("It took %fs to load the dataset\n", loadDuration);
+
+    // [2] Data processing
+    double procBegin = cpuSecond();
+
+    for (int i = 0; i < dataset.size(); ++i)
     {
-        CSVRow row = (*loop);
+        CSVRow row = dataset[i];
         int num_pers_killed = row.getNumPersonKilled();
         int week = getWeek(row[DATE]);
         int year = getYear(row[DATE]) - 2012;
@@ -60,15 +72,25 @@ int main()
         }
     }
 
-    double duration = cpuSecond() - begin;
-    printf("It took %fs to calculate it\n", duration);
+    double procDuration = cpuSecond() - procBegin;
+    printf("It took %fs to process the data\n", procDuration);
 
-    // // Print result based on year
-    // int year = 2016; // Choose year to print
-    // typedef map<int, int>::const_iterator MapIterator;
-    // for (MapIterator iter = accPerWeek[year - 2012].begin(); iter != accPerWeek[year - 2012].end(); iter++)
-    // {
-    //     cout << "(" << year << ")Week: " << iter->first << "\t\t\t Num. person killed: ";
-    //     cout << iter->second << endl;
-    // }
+    // [3] Output result
+    double outBegin = cpuSecond();
+
+    typedef map<int, int>::const_iterator MapIterator;
+    for (int i = 0; i < accPerWeek.size(); ++i)
+    {
+        for (MapIterator iter = accPerWeek[i].begin(); iter != accPerWeek[i].end(); iter++)
+        {
+            cout << "(" << (i + 2012) << ")Week: " << iter->first << "\t\t\t Num. person killed: ";
+            cout << iter->second << endl;
+        }
+    }
+
+    double outDuration = cpuSecond() - outBegin;
+    printf("It took %fs to output the result\n", outDuration);
+
+    double overallDuration = cpuSecond() - overallBegin;
+    printf("Overall process duration is %fs", overallDuration);
 }
