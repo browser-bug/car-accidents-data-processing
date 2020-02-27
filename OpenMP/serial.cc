84cdcca18805474b31962a50fce5d856bb9c93ee
bernardo <bernardo.menicagli@gmail.com>
2020-02-27 02:26:35 +0100

    New mods.

--- OpenMP/serial.cc
+++ OpenMP/serial.cc
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <string>
+#include <time.h>
 
 #include "utilities/CSVIterator.h"
 
@@ -23,19 +25,32 @@ int main()
     //     cout << endl;
     // }
 
-    // CSVRow row;
-    // file >> row >> row;
+    // map<string, int> accPerDay; // map { Day ; num_accidents }
 
-    // Number of lethal accidents per week throughout the entire dataset. Using index: NUMBER_OF_PERSONS_KILLED
-    for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
-    {
-        CSVRow row = (*loop);
-        int num_pers_killed = row.getNumPersonKilled();
+    // // Number of lethal accidents per week throughout the entire dataset. Using index: NUMBER_OF_PERSONS_KILLED
+    // for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
+    // {
+    //     CSVRow row = (*loop);
+    //     int num_pers_killed = row.getNumPersonKilled();
 
-        if (num_pers_killed > 0)
-        {
-            row.print();
-            cout << endl;
-        }
-    }
+    //     if (num_pers_killed > 0)
+    //     {
+    //         auto it = accPerDay.find(row[DATE]);
+    //         if (it != accPerDay.end())
+    //             it->second += num_pers_killed;
+    //         else
+    //             accPerDay.insert({row[DATE], num_pers_killed});
+    //     }
+    // }
+
+    // typedef map<string, int>::const_iterator MapIterator;
+    // for (MapIterator iter = accPerDay.begin(); iter != accPerDay.end(); iter++)
+    // {
+    //     cout << "Key: " << iter->first << endl
+    //          << "Values: ";
+    //     cout << iter->second << endl;
+    //     // typedef int ::const_iterator ListIterator;
+    //     // for (ListIterator list_iter = iter->second.begin(); list_iter != iter->second.end(); list_iter++)
+    //     //     cout << " " << *list_iter << endl;
+    // }
 }
