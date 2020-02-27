66b900edb2d017f1a7d6e648e15daf325d9ab25d
Bernardo <bernardo.menicagli@gmail.com>
2020-02-27 18:27:07 +0100

    Implemented serially the num. of deaths per week

--- OpenMP/serial.cc
+++ OpenMP/serial.cc
@@ -5,7 +5,7 @@
 #include <map>
 #include <algorithm>
 #include <string>
-#include <time.h>
+#include <iomanip>
 
 #include "utilities/CSVIterator.h"
 
@@ -13,10 +13,15 @@ using namespace std;
 
 int main()
 {
-    string csv_path = "dataset/data_test.csv";
+    string csv_path = "dataset/NYPD_Motor_Vehicle_Collisions.csv";
     ifstream file(csv_path);
 
-    // Print the entire dataset
+    // // Testing on one single row
+    // CSVRow row;
+    // file >> row;
+    // row.print();
+
+    // // Print the entire dataset
     // CSVRow row;
     // for (CSVIterator loop(file); loop != CSVIterator(); loop++)
     // {
@@ -25,32 +30,39 @@ int main()
     //     cout << endl;
     // }
 
-    // map<string, int> accPerDay; // map { Day ; num_accidents }
+    // map { weekNumber ; num_accidents }
+    vector<map<int, int>> accPerWeek = {
+        map<int, int>(), // 2012
+        map<int, int>(), // 2013
+        map<int, int>(), // 2014
+        map<int, int>(), // 2015
+        map<int, int>(), // 2016
+        map<int, int>()  // 2017
+    };
 
-    // // Number of lethal accidents per week throughout the entire dataset. Using index: NUMBER_OF_PERSONS_KILLED
-    // for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
-    // {
-    //     CSVRow row = (*loop);
-    //     int num_pers_killed = row.getNumPersonKilled();
+    // Number of lethal accidents per week throughout the entire dataset. Using index: NUMBER_OF_PERSONS_KILLED
+    for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
+    {
+        CSVRow row = (*loop);
+        int num_pers_killed = row.getNumPersonKilled();
+        int week = getWeek(row[DATE]);
+        int year = getYear(row[DATE]) - 2012;
 
-    //     if (num_pers_killed > 0)
-    //     {
-    //         auto it = accPerDay.find(row[DATE]);
-    //         if (it != accPerDay.end())
-    //             it->second += num_pers_killed;
-    //         else
-    //             accPerDay.insert({row[DATE], num_pers_killed});
-    //     }
-    // }
+        if (num_pers_killed > 0)
+        {
+            auto it = accPerWeek[year].find(week);
+            if (it != accPerWeek[year].end())
+                it->second += num_pers_killed;
+            else
+                accPerWeek[year].insert({week, num_pers_killed});
+        }
+    }
 
-    // typedef map<string, int>::const_iterator MapIterator;
-    // for (MapIterator iter = accPerDay.begin(); iter != accPerDay.end(); iter++)
-    // {
-    //     cout << "Key: " << iter->first << endl
-    //          << "Values: ";
-    //     cout << iter->second << endl;
-    //     // typedef int ::const_iterator ListIterator;
-    //     // for (ListIterator list_iter = iter->second.begin(); list_iter != iter->second.end(); list_iter++)
-    //     //     cout << " " << *list_iter << endl;
-    // }
+    int year = 2016; // Choose year to print
+    typedef map<int, int>::const_iterator MapIterator;
+    for (MapIterator iter = accPerWeek[year - 2012].begin(); iter != accPerWeek[year - 2012].end(); iter++)
+    {
+        cout << "(" << year << ")Week: " << iter->first << "\t\t\t Num. person killed: ";
+        cout << iter->second << endl;
+    }
 }
