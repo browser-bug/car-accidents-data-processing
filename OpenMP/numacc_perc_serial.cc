9276b1732ece3a7b6b2e4dfa4ccba37ff97eb99b
bernardo <bernardo.menicagli@gmail.com>
2020-02-28 13:59:57 +0100

    Working implementation to infer data for task2.

--- OpenMP/numacc_perc_serial.cc
+++ OpenMP/numacc_perc_serial.cc
@@ -13,50 +13,61 @@ using namespace std;
 
 int main()
 {
-    string csv_path = "dataset/data_test.csv";
+    // complete dataset NYPD_Motor_Vehicle_Collisions
+    string csv_path = "dataset/NYPD_Motor_Vehicle_Collisions.csv";
     ifstream file(csv_path);
 
     // // Testing on one single row
     // CSVRow row;
     // file >> row;
     // row.print();
+    // cout << endl;
+    // vector<string> cfs = row.getContributingFactors();
+    // cout << cfs.size() << endl;
+    // for (auto it = cfs.begin(); it != cfs.end(); ++it)
+    //     cout << *it << endl;
 
     // // Print the entire dataset
-    // CSVRow row;
     // for (CSVIterator loop(file); loop != CSVIterator(); loop++)
     // {
+    //     CSVRow row;
     //     row = (*loop);
+    //     row.print();
     //     cout << endl;
     // }
 
-    map<string, pair<int, int>> AccAndPerc; // map { contributingFactor ; {numAccidents, percNumDeaths} }
+    map<string, pair<int, double>> AccAndPerc; // map { contributingFactor ; {numAccidents, percNumDeaths} }
 
     for (CSVIterator loop(file); loop != CSVIterator(); loop++)
     {
         CSVRow row = (*loop);
 
-        string cf = row[CONTRIBUTING_FACTOR_VEHICLE_1];
-        int lethal = (row.getNumPersonKilled() > 0) ? 1 : 0;
+        vector<string> cfs = row.getContributingFactors();
+        if (cfs.empty())
+            continue;
+        double lethal = (row.getNumPersonKilled() > 0) ? 1 : 0;
 
-        auto it = AccAndPerc.find(cf);
-        if (it == AccAndPerc.end())
+        // For every CF found I insert it into the map
+        for (auto itr = cfs.begin(); itr != cfs.end(); ++itr)
         {
-            (it->second.first)++;
-            if (lethal)
-                (it->second.second)++;
+            auto it = AccAndPerc.find(*itr);
+            if (it != AccAndPerc.end())
+            {
+                (it->second.first)++;
+                if (lethal)
+                    (it->second.second)++;
+            }
+            else
+                AccAndPerc.insert({row[CONTRIBUTING_FACTOR_VEHICLE_1], {1, lethal}});
         }
-        else
-            AccAndPerc.insert({row[CONTRIBUTING_FACTOR_VEHICLE_1], {1, lethal}});
-
-        cout << endl;
     }
 
-    typedef map<string, pair<int, int>>::const_iterator MapIterator;
+    typedef map<string, pair<int, double>>::const_iterator MapIterator;
     for (MapIterator iter = AccAndPerc.begin(); iter != AccAndPerc.end(); iter++)
     {
-        cout << iter->first << "\t\t\t Num. of accidents: ";
-        cout << iter->second.first;
-        cout << "\t Perc. lethal accidents: ";
-        cout << (iter->second.second / iter->second.first) << endl;
+        cout << iter->first << endl
+             << "\t\tNum. of accidents: " << iter->second.first
+             << "\t\t\tPerc. lethal accidents: " << (iter->second.second / iter->second.first)
+             << endl;
     }
 }
