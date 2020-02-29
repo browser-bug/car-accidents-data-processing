8bca7ddfdac5af9c29131f8c9bd467aacebf5ebf
bernardo <bernardo.menicagli@gmail.com>
2020-02-29 10:48:26 +0100

    Fixed and working example.

--- OpenMP/accperweek_serial.cc
+++ OpenMP/accperweek_serial.cc
@@ -13,22 +13,9 @@ using namespace std;
 
 int main()
 {
+    // complete dataset NYPD_Motor_Vehicle_Collisions
     string csv_path = "dataset/NYPD_Motor_Vehicle_Collisions.csv";
     ifstream file(csv_path);
-
-    // // Testing on one single row
-    // CSVRow row;
-    // file >> row;
-    // row.print();
-
-    // // Print the entire dataset
-    // CSVRow row;
-    // for (CSVIterator loop(file); loop != CSVIterator(); loop++)
-    // {
-    //     row = (*loop);
-    //     row.print();
-    //     cout << endl;
-    // }
 
     // map { weekNumber ; numLethalAccidents }
     vector<map<int, int>> lethAccPerWeek = {
@@ -45,12 +32,14 @@ int main()
     // [1] Loading data from file
     double loadBegin = cpuSecond();
 
+    // CSVRow* dataset = new CSVRow[955928];
     vector<CSVRow> dataset;
-    for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
+    CSVIterator loop(file);
+    ++loop; // skip the header
+    for (; loop != CSVIterator(); ++loop)
         dataset.push_back((*loop));
 
     double loadDuration = cpuSecond() - loadBegin;
-    printf("It took %fs to load the dataset\n", loadDuration);
 
     // [2] Data processing
     double procBegin = cpuSecond();
@@ -58,39 +47,55 @@ int main()
     for (unsigned int i = 0; i < dataset.size(); ++i)
     {
         CSVRow row = dataset[i];
-        int num_pers_killed = row.getNumPersonKilled();
+        int num_pers_killed = row.getNumPersonsKilled();
         int week = getWeek(row[DATE]);
+        int month = getMonth(row[DATE]);
         int year = getYear(row[DATE]) - 2012;
 
-        if (num_pers_killed > 0)
+        // If I'm week = 1 and month = 12, this means I belong to the first week of the next year.
+        // If I'm week = (52 or 53) and month = 01, this means I belong to the last week of the previous year.
+        if (week == 1 && month == 12)
+            year++;
+        else if ((week == 52 || week == 53) && month == 1)
+            year--;
+
+        if (num_pers_killed > 0) // We have a lethal accident
         {
             auto it = lethAccPerWeek[year].find(week);
             if (it != lethAccPerWeek[year].end())
-                it->second += num_pers_killed;
+                (it->second)++;
             else
-                lethAccPerWeek[year].insert({week, num_pers_killed});
+                lethAccPerWeek[year].insert({week, 1});
         }
     }
 
     double procDuration = cpuSecond() - procBegin;
-    printf("It took %fs to process the data\n", procDuration);
 
     // [3] Output result
     double outBegin = cpuSecond();
 
+    int totalWeeks = 0;
+    int totalAccidents = 0;
     typedef map<int, int>::const_iterator MapIterator;
     for (unsigned int i = 0; i < lethAccPerWeek.size(); ++i)
     {
         for (MapIterator iter = lethAccPerWeek[i].begin(); iter != lethAccPerWeek[i].end(); iter++)
         {
-            cout << "(" << (i + 2012) << ")Week: " << iter->first << "\t\t\t Num. person killed: ";
+            cout << "(" << (i + 2012) << ")Week: " << iter->first << "\t\t\t Num. lethal accidents: ";
             cout << iter->second << endl;
+            totalAccidents += iter->second;
         }
+        totalWeeks += lethAccPerWeek[i].size();
     }
 
     double outDuration = cpuSecond() - outBegin;
-    printf("It took %fs to output the result\n", outDuration);
 
     double overallDuration = cpuSecond() - overallBegin;
-    printf("Overall process duration is %fs", overallDuration);
+
+    cout << "Overall process duration is " << overallDuration << "s\n";
+    cout << "It took " << loadDuration << "s to load the dataset\n";
+    cout << "It took " << procDuration << "s to process the data\n";
+    cout << "It took " << outDuration << "s to output the result\n";
+    cout << "Total weeks: " << totalWeeks << endl;
+    cout << "Total accidents: " << totalAccidents << endl;
 }
