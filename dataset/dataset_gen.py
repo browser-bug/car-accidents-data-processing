cb2fef01d6955a69dd5a294e5f5892ea9c6062cc
Bernardo <bernardo.menicagli@gmail.com>
2020-04-02 20:04:27 +0200

    Small fixes.

--- dataset/dataset_gen.py
+++ dataset/dataset_gen.py
@@ -5,8 +5,8 @@ import sys
 #
 # ASSUMPTION: dataset input file number of rows order is 1M
 #
-if len(sys.argv) != 3:
-    print 'Express num. of rows in M: ', sys.argv[0], '<input_file_path> <num_of_rows>'    
+if len(sys.argv) < 3:
+    print 'Express num. of rows in M: ', sys.argv[0], '<input_file_path> [<num_of_rows>]+'    
     exit(-1)
 
 dataset_path = sys.argv[1]
@@ -18,27 +18,28 @@ print "Shuffling dataframe..."
 dataframe_input = dataframe_input.sample(frac=1).reset_index(drop=True) # shuffle rows
 print "Done."
 
-dataframe_output = pd.DataFrame()
+for k in range(2, len(sys.argv)):
+    dataframe_output = pd.DataFrame()
 
-num_rows = float(sys.argv[2]) # number of rows to generate
-decimal_part, integer_part = modf(num_rows)
+    num_rows = float(sys.argv[k]) # number of rows to generate
+    decimal_part, integer_part = modf(num_rows)
 
-print "Generating new dataset with %sM rows... " % (sys.argv[2])
-dataframe_output = dataframe_input
+    print "\nGenerating new dataset with %sM rows... " % (sys.argv[k])
+    dataframe_output = dataframe_input
 
-dfs = []
-if decimal_part != 0:
-    take_num = int(decimal_part * 10**6);
-    df_last_rows = dataframe_input.tail(take_num) # take last n rows
-    dfs.append(df_last_rows)
-if integer_part != 1:
-    for _ in range(int(integer_part)):
-        dfs.append(dataframe_input)
+    dfs = []
+    if decimal_part != 0:
+        take_num = int(decimal_part * 10**6);
+        df_last_rows = dataframe_input.tail(take_num) # take last n rows
+        dfs.append(df_last_rows)
+    if integer_part != 1:
+        for _ in range(int(integer_part)):
+            dfs.append(dataframe_input)
 
-if len(dfs) > 0:
-    dataframe_output = pd.concat(dfs, ignore_index=True)
-print "Done."
+    if len(dfs) > 0:
+        dataframe_output = pd.concat(dfs, ignore_index=True)
+    print "Done."
 
-print "Writing output file..."
-dataframe_output.to_csv('collisions_' + sys.argv[2] + 'M.csv', index=False)
-print "Done."
+    print "Writing output file..."
+    dataframe_output.to_csv('collisions_' + sys.argv[k] + 'M.csv', index=False)
+    print "Done."
