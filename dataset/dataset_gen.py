d66e0d2170aa50ae7da51dee150d5817c6740409
Bernardo <bernardo.menicagli@gmail.com>
2020-04-02 15:57:42 +0200

    Added dataset generator script.

---
+++ dataset/dataset_gen.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from math import modf
+import sys
+
+#
+# ASSUMPTION: dataset input file number of rows order is 1M
+#
+if len(sys.argv) != 3:
+    print 'Express num. of rows in M: ', sys.argv[0], '<input_file_path> <num_of_rows>'    
+    exit(-1)
+
+dataset_path = sys.argv[1]
+print "Reading input file..."
+dataframe_input = pd.read_csv(dataset_path)
+print "Done."
+
+print "Shuffling dataframe..."
+dataframe_input = dataframe_input.sample(frac=1).reset_index(drop=True) # shuffle rows
+print "Done."
+
+dataframe_output = pd.DataFrame()
+
+num_rows = float(sys.argv[2]) # number of rows to generate
+decimal_part, integer_part = modf(num_rows)
+
+print "Generating new dataset with %sM rows... " % (sys.argv[2])
+dataframe_output = dataframe_input
+
+dfs = []
+if decimal_part != 0:
+    take_num = int(decimal_part * 10**6);
+    df_last_rows = dataframe_input.tail(take_num) # take last n rows
+    dfs.append(df_last_rows)
+if integer_part != 1:
+    for _ in range(int(integer_part)):
+        dfs.append(dataframe_input)
+
+if len(dfs) > 0:
+    dataframe_output = pd.concat(dfs, ignore_index=True)
+print "Done."
+
+print "Writing output file..."
+dataframe_output.to_csv('collisions_' + sys.argv[2] + 'M.csv', index=False)
+print "Done."
