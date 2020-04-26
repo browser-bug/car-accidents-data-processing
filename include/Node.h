3a49b93029605d1acaa56a9e556d1ae267c8fdb8
Bernardo <bernardo.menicagli@gmail.com>
2020-04-26 17:15:38 +0200

    Added generic node interface.

---
+++ include/Node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <mpi.h>
+
+#include "../src/utils/include/data_types.h"
+#include "../src/utils/csv_row/include/CSVIterator.h"
+
+class Node
+{
+public:
+    Node(int myRank, MPI_Comm myCommunicator) : rank(myRank), comm(myCommunicator) {}
+    ~Node() {}
+
+protected:
+    int rank;
+    MPI_Comm comm;
+};
+
+#endif
